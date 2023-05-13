b86702fe44526482a362a08d5ebf06ef1177b265
jfoltan1 <juliusfoltan@gmail.com>
2023-05-13 10:54:25 +0200

    Writing to array now works.

--- push_swap.c
+++ push_swap.c
@@ -1,37 +1,35 @@
 #include <stdio.h>
 #include "libft/libft.h"
-/* initialize stacks */
-char *init_and_write_stack_a(int argc, char **argv)
+/* initialize stack a */
+int	*init_and_write_stack_a(int argc, char **argv)
 {
-	char *stack_a;
+	int	*stack_a;
 	int	i;
 
 	i = 1;
-	stack_a = malloc(sizeof(int) * argc);
+	stack_a = ft_calloc((argc - 1), sizeof(int));
 	while (i < argc)
 	{
-		stack_a[0] = ft_atoi(argv[i]);
+		stack_a[i - 1] = ft_atoi(argv[i]);
 		i++;
 	}
 	return (stack_a);
 }
-char *init_stack_b(int argc)
-{
-	char *stack_b;
 
-	stack_b = malloc(sizeof(int) * argc);
-	return (stack_b);
-}
-char *push_swap(int argc, char **argv)
+int *push_swap(int argc, char **argv)
 {
-	char *stack_a;
-	// char *stack_b;
+	int *stack_a;
 
 	stack_a = init_and_write_stack_a(argc,argv);
 	return(stack_a);
 }
 int main(int argc, char **argv)
 {
-	printf("%s",push_swap(argc,argv));
+	int	i;
+	i = 0;
+	while (i < argc-1)
+	{
+		printf("%d ",(push_swap(argc,argv))[i]);
+		i++;
+	}
 }
-/* dopice nejde to nepise to do array */
